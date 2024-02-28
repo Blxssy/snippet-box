d18c327d2de40fc69af081e042350bc1b4626155
Blxssy <roma.asav@gmail.com>
2024-02-28 22:20:59 +0300

    -addr

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
+	log.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
